d64c410d3f41fed49a7b37c1114a6a600ea2fd59
giacomelli <giacomelli@gmail.com>
2018-12-15 04:29:19 -0200

    Abrindo a página de histórico do AppVeyor ao final do comando

--- src/JDBot.ConsoleApp/Commands/Sites/ReleaseSubcommand.cs
+++ src/JDBot.ConsoleApp/Commands/Sites/ReleaseSubcommand.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using JDBot.Domain.Sites;
 using JDBot.Infrastructure.Framework;
 using JDBot.Infrastructure.Git;
 using JDBot.Infrastructure.IO;
+using JDBot.Infrastructure.Net;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace JDBot.ConsoleApp.Commands.Sites
@@ -40,7 +42,7 @@ namespace JDBot.ConsoleApp.Commands.Sites
             var version = await releaser.ReleaseAsync(Message, Patch);
 
             Logger.Info($"Versão: {version} liberada no master do GitHub.");
-            Process.Start(new ProcessStartInfo { FileName = "https://ci.appveyor.com/project/giacomelli/jogosdaqui-github-io-jekyll/history", UseShellExecute = true });
+            AppVeyorSitePublicationProxy.OpenHistoryPage();
 
             return 0;
         }
